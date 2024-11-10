0864f1b1a82589378291a44dafc2e4645130b632
Zsolt Döme <zsoltdome0005@gmail.com>
2024-11-10 21:14:46 +0100

    Cleanup Pet data

--- IntegrationTests/src/Data/Pet.php
+++ IntegrationTests/src/Data/Pet.php
@@ -2,8 +2,6 @@
 
 namespace PetStore\Tests\Data;
 
-use Symfony\Component\Serializer\Annotation\SerializedPath;
-
 /**
  * Data for representing a pet.
  *
@@ -26,28 +24,9 @@ final class Pet
     public array $photoUrls = [];
 
     /** @var Tag[] Tags. */
-    #[SerializedPath('[tags][tag]')]
     public array $tags = [];
 
     /** @var string Status. */
     public string $status;
 
-    /**
-     * Custom setter for the photo URLs.
-     *
-     * @param string[] $photoUrls
-     *
-     * @return void
-     */
-    public function setPhotoUrls(array $photoUrls): void
-    {
-        // Hot fix for a bug where the serializer serializes empty arrays as empty item tag, and after de-serializations, an empty string is returned as the first item
-        if(count($photoUrls) === 1 && empty($photoUrls[0]))
-        {
-            $this->photoUrls = [];
-            return;
-        }
-
-        $this->photoUrls = $photoUrls;
-    }
 }
